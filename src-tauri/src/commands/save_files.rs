e86d212a914019f0a10f586d6366a7169cd76ff5
Itai Fish <chessandpotatoes@gmail.com>
2024-12-28 15:57:16 -0500

    this may be better

--- src-tauri/src/commands/save_files.rs
+++ src-tauri/src/commands/save_files.rs
@@ -1,6 +1,7 @@
 use std::{fs::{self}, io::Error};
 use serde::Deserialize;
 use rayon::iter::{IndexedParallelIterator, IntoParallelRefIterator, ParallelIterator};
+use tauri::async_runtime::spawn_blocking;
 
 #[derive(Deserialize)]
 pub struct FileData {
@@ -10,13 +11,18 @@ pub struct FileData {
 
 #[tauri::command]
 pub async fn save_files(data: Vec<FileData>) -> bool {
-	let results: Vec<Result<(), Error>> = data
-		.par_iter()
-		.enumerate()
-		.map(|(_, file_data)| {
-			fs::write(&file_data.path, &file_data.contents)
-		})
-		.collect();
+	let results = spawn_blocking(move || { 
+			let data: Vec<Result<(), Error>> = data
+				.par_iter()
+				.enumerate()
+				.map(|(_, file_data)| {
+					fs::write(&file_data.path, &file_data.contents)
+				})
+				.collect();
+			data
+		}
+	).await.unwrap();
+
 	let failures = results
 		.iter()
 		.any(Result::is_err);
