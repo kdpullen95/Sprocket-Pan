2b67a129843537f85b05ab3e1ba7d4225ad336af
Itai Fish <chessandpotatoes@gmail.com>
2025-01-26 21:50:56 -0500

    add secondary infographic for slow workspaces

--- src/components/root/overlays/LoadingOverlay.tsx
+++ src/components/root/overlays/LoadingOverlay.tsx
@@ -1,10 +1,23 @@
 import { selectIsLoading } from '@/state/ui/selectors';
-import { Stack, CircularProgress } from '@mui/joy';
+import { Stack, CircularProgress, Typography } from '@mui/joy';
+import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 
 export function LoadingOverlay() {
 	const isLoading = useSelector(selectIsLoading);
-
+	const [showStillWorkingOnItDisplay, setShowStillWorkingOnItDisplay] = useState(false);
+	useEffect(() => {
+		if (isLoading) {
+			// after 8 seconds of loading, if we're still loading set the display to true
+			setTimeout(() => {
+				if (isLoading) {
+					setShowStillWorkingOnItDisplay(true);
+				}
+			}, 8_000);
+		} else {
+			setShowStillWorkingOnItDisplay(false);
+		}
+	}, [isLoading]);
 	return (
 		<Stack
 			alignItems="center"
@@ -31,6 +44,13 @@ export function LoadingOverlay() {
 				}}
 				variant="solid"
 			/>
+			<Typography level="h1">Loading Workspace...</Typography>
+			{showStillWorkingOnItDisplay && (
+				<Typography level="h2">
+					This may take a while, (likely due to a large history), but Sprocket Pan has not frozen and your workspace
+					will load shortly.
+				</Typography>
+			)}
 		</Stack>
 	);
 }
