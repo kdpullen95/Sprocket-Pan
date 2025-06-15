81e285e1c96aeafadafc14deecdd2de781c44666
KateLynn Pullen <kdpullen95@gmail.com>
2025-06-15 10:39:39 -0400

    state persists

--- src/components/sidebar/SideDrawerContent.tsx
+++ src/components/sidebar/SideDrawerContent.tsx
@@ -1,22 +1,29 @@
+import { Sheet, Stack } from '@mui/joy';
 import { EnvironmentsFileSystem } from './file-system/environment/EnvironmentsFileSystem';
 import { ScriptsFileSystem } from './file-system/script/ScriptsFileSystem';
 import { ServicesFileSystem } from './file-system/service/ServicesFileSystem';
 import { WorkspacesFileSystem } from './file-system/workspace/WorkspacesFileSystem';
 import { SidebarTabs } from './types';
 
+const tabs = [
+	{ content: <WorkspacesFileSystem />, value: SidebarTabs.Workspaces },
+	{ content: <EnvironmentsFileSystem />, value: SidebarTabs.Environments },
+	{ content: <ScriptsFileSystem />, value: SidebarTabs.Scripts },
+	{ content: <ServicesFileSystem />, value: SidebarTabs.Services },
+];
+
 interface SideDrawerContentProps {
 	tab: SidebarTabs;
 }
 
 export function SideDrawerContent({ tab }: SideDrawerContentProps) {
-	switch (tab) {
-		case SidebarTabs.Workspaces:
-			return <WorkspacesFileSystem />;
-		case SidebarTabs.Environments:
-			return <EnvironmentsFileSystem />;
-		case SidebarTabs.Scripts:
-			return <ScriptsFileSystem />;
-		case SidebarTabs.Services:
-			return <ServicesFileSystem />;
-	}
+	return (
+		<>
+			{tabs.map(({ value, content }) => (
+				<Stack key={value} component={Sheet} display={tab === value ? 'inherit' : 'none'} minHeight="1px" flex={1}>
+					{content}
+				</Stack>
+			))}
+		</>
+	);
 }
